872a9283ac9ea1e69168480ac80be8e87944cd63
wei chen <weichen9083@gmail.com>
2019-08-21 02:47:01 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,6 +2,10 @@ def using_while
   levitation_force = 6
 	
   #your code here
+  while levitation_force < 10 
+  puts "we should increment our levitation force by 1"
+  levitation_force += 1
+end 
 end
 
 
