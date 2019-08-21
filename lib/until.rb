950f32eb41dd12e3c04563de7594186dec6f4441
wei chen <weichen9083@gmail.com>
2019-08-21 02:47:49 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,10 @@
 def using_until
   levitation_force = 6
   #your code here
-  until levitation_force
+  until levitation_force == 10 
+  puts " we should increment our levitation force by 1"
+  levitation_force += 1
+end 
     
 end
 
